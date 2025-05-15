29ed4d33e8af15ed98ac0ee02f5b1d0b4023ec9c
Tcooper4 <tacooper608@gmail.com>
2025-05-15 14:22:22 -0400

    Fixed import error for model_factory module

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Upgrading pip..."
-pip install --upgrade pip
-
-echo "Installing OpenAI..."
-pip install openai==0.27.4 --force-reinstall
-
-echo "Installing other dependencies..."
-pip install -r requirements.txt --upgrade --force-reinstall
