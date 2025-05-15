b383ed8211ff152433ea74a3d0185b69591c613f
Tcooper4 <tacooper608@gmail.com>
2025-05-15 13:35:09 -0400

    Fixed import error for model_factory module

--- ui/app.py
+++ ui/app.py
@@ -1,4 +1,8 @@
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 import yaml
 from core.model_factory import create_model
